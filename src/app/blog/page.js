1be8b1e21d1aba66ca64cdecb97201c3caf686c8
unknown <freelancernayem.me@gmail.comm>
2024-08-29 20:46:22 +0600

    '3'

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Blogpage = () => {
+const Blogpage = async () => {
+    let data = await fetch('https://jsonplaceholder.typicode.com/users');
+    let blogs = await data.json()
+    console.log(blogs);
+
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
             blog page
+            {
+                blogs.map((blog) => (
+                    <div>
+                        <h2>{blog.name}</h2>
+                        <h3>{blog.username}</h3>
+                    </div>
+                ))
+            }
         </div>
     );
 };
